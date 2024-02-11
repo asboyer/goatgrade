bfaacc0bc1178f6e996aa0cb663aa1f4fd9405a9
Andrew Boyer <asboyer@gmail.com>
2024-02-10 21:04:13 -0500

    setup auto script

--- goatgrade.py
+++ goatgrade.py
@@ -1,30 +1,13 @@
+import daily_update
+
 # scrape
-    # scrape_player
-    # scrape_stats
-    # scrape_team
+daily_update.update_players()
+daily_update.update_standings()
+daily_update.update_teams()
 
-# grade
-    # grade_player
-    # grade_team
+# internal organization
+daily_update.update_internal_info()
 
-# daily_update
-    # update_teams
-    # update_players
-    # grade_teams
-    # grade_players
-
-import daily_update, grade
-
-update_players()
-update_standings()
-update_teams()
-
-update_internal_info()
-
-ranks = grade.grade_players(2024, date_string)
-with open(f"data/stat/players/grades/{date_string}.json", "w+", encoding="utf8") as file:
-    file.write(json.dumps(ranks, ensure_ascii=False, indent=4))
-    
-ranks = grade.grade_team(2024, date_string)
-with open(f"data/team/grades/{date_string}.json", "w+", encoding="utf8") as file:
-    file.write(json.dumps(ranks, ensure_ascii=False, indent=4))
+# grades
+daily_update.update_grades_players()
+daily_update.update_grades_teams()
